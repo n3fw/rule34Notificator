1be011b0419f877261a6cfaa00a299121a514d61
feed3r <113500716+n3fw@users.noreply.github.com>
2024-03-26 22:31:11 +0100

    added scripts for main.py

---
+++ py_scripts/page_get.py
@@ -0,0 +1,144 @@
+from http.client import InvalidURL
+from urllib.error import URLError
+import urllib.request
+import urllib.parse
+import ssl
+import socket
+import rule34Py
+
+class PageGet:
+    def __init__(self):
+        self.url = 'https://rule34.xxx/'
+        self.tag_amount = 0
+        self.newposts = 0
+        self.number_page = 0
+        self.tag = ""
+        self.hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+        }
+        self.context = ssl._create_unverified_context()
+        self.r34 = rule34Py.rule34Py()
+
+    
+    def get_url(self):
+        return self.url
+    
+    def get_tag_amount(self):
+        return self.tag_amount
+
+    def get_newposts(self):
+        return self.newposts
+    
+    def get_tag(self):
+        return self.tag
+    
+    def get_number_page(self):
+        return self.number_page
+    
+    def add_tag(self, given_tag: str) -> bool:
+        """
+        checks if the tag given raises an error (see error_handle())
+
+        :return True or False depending on the outcome 
+        :if True changes self.url 
+        """
+        querystring = "index.php?page=post&s=list&tags=" + given_tag
+        verif = "{}{}".format(self.url, querystring)
+        req = urllib.request.Request(verif, headers = self.hdr)
+        if (self.error_handle(req, self.context)):
+            self.url = verif
+            self.tag = given_tag
+            return True
+        else:
+            return False
+    
+    def net_check(self) -> bool:
+        """
+        checks if connexion to rule34 is successful
+
+        :uses error_handle()
+        :return True if success, else False (webUrl.getcode() == 200 -> success)
+        """
+        req = urllib.request.Request(self.url, headers = self.hdr)
+
+        if (self.error_handle(req, self.context)):
+            webUrl = urllib.request.urlopen(req, context = self.context)
+            if (webUrl.getcode() == 200):
+                return True
+            else:
+                return False
+        return False
+        
+    def error_handle(self, link: urllib.request.Request, cont: ssl.SSLContext) -> bool:
+        """
+        handles error that may happen with encoding the url to a tag
+
+        :return False if error, else True
+        """
+        try:
+            url = urllib.request.urlopen(link, context=cont)
+        except InvalidURL:
+            return False
+        except URLError:
+            return False
+        return True
+    
+    def page_amount(self):
+        """
+        determines how many pages of posts there are with the given tag
+
+        :return None
+        :changes self.number_page
+        """
+        numberpage =0
+        while True:
+            if len(self.r34.search([self.tag], page_id=numberpage, limit=42)) == 42:
+                numberpage += 100
+            else:
+                break
+        add = 25
+        numberpage -= 50
+        while True:
+            if len(self.r34.search([self.tag], page_id=numberpage, limit=42)) != 0 and len(self.r34.search([self.tag], page_id=numberpage + 1, limit=42)) == 0:
+                self.number_page = numberpage + 1
+                break
+            if len(self.r34.search([self.tag], page_id=numberpage, limit=42)) != 0:
+                numberpage += add
+            else:
+                numberpage -= add
+            if add != 1:
+                add = add // 2
+    
+    def number_post_by_tag(self):
+        """
+        determines the number of posts that have the given tag
+
+        :return None
+        :changes self.tag_amount
+        """
+        self.tag_amount = (self.number_page - 1)*42 +len(self.r34.search([self.tag], page_id=(self.number_page-1), limit = 42))
+    
+    def calc_new_posts(self, old_save: int, new_save: int):
+            """
+            determines the amount of new posts since last save
+
+            :return None
+            :changes self.newposts
+            """
+            self.newposts = new_save - old_save
+
+    def is_tag_none(self) -> bool:
+        """
+        checks if tag exists or not
+
+        :return True if it doesn't exist, else False
+        """
+        if len(self.r34.search([self.tag], page_id=0, limit=42)) == 0:
+            return True
+        else:
+            return False
---
+++ py_scripts/run_app.py
@@ -0,0 +1,119 @@
+import page_get as pg
+import save_data as sd
+import ui as ui
+
+class RunApp():
+    def __init__(self) -> None:
+        self.page = pg.PageGet()
+        self.data = sd.DataSave()
+        self.ui = ui.UI()
+        self.for_notif = []
+        self.notifs = []
+    
+    def get_for_notif(self):
+        return self.for_notif
+    
+    def get_notifs(self):
+        return self.notifs
+
+    def run(self):
+        if self.data.is_first_launch():
+            self.data.create_config()
+        if self.page.net_check() == False:
+            self.ui.print_message("Couldn't connect to rule34 ! Try again", "Error")
+            exit()
+        self.ui.main_window()
+        WhatToDo = self.ui.get_action_id()
+        if WhatToDo == 0:
+            if self.ui.get_tag_to_add() != None: 
+                tag = self.page.add_tag(self.ui.get_tag_to_add())
+                if self.page.is_tag_none() or tag == False:
+                    self.ui.print_message("This tag doesn't exist or uses invalid caracters", "TagNotFound")
+                else:
+                    self.page.page_amount()
+                    self.page.number_post_by_tag()
+                    setting = self.ui.get_settings()
+                    posts = str(self.page.get_tag_amount())
+
+                    self.data.add_fav_tag(self.page.get_tag(), setting, posts)
+                    self.for_notif.append([self.page.get_tag(), "0", 0])
+                    self.ui.print_message("Tag succesfully added !", "Success")
+        
+        elif WhatToDo == 1:
+            tag = self.ui.get_tag_to_remove()
+            if self.data.exist_or_not(tag):
+                self.data.remove_fav_tag(tag)
+                self.ui.print_message("Tag succesfully removed.", "Success")
+            else:
+                self.ui.print_message("Tag is not saved as favorite !", "TagNotFound")
+
+        elif WhatToDo == 2:
+            self.data.config.read("config.ini")
+            if self.data.config.items("tags") == [] or self.data.config.items("tags") == None:
+                self.ui.print_message("No tags saved as favorites !", "TagNotFound")
+            else:
+                self.ui.button_notif_clicked(self.notifs)
+
+    def update(self):
+        """
+        changes the values of posts saved to the current amount
+
+        :changes the config.ini file
+        """
+        self.data.config.read("config.ini")
+        list_tag = self.data.config.items("tags")
+        for i in range (len(list_tag)):
+            self.page.add_tag(list_tag[i][1])
+            self.page.page_amount()
+            self.page.number_post_by_tag()
+            self.for_notif[i][2] = self.page.get_tag_amount()
+            self.data.config.set("save_posts", "val"+f"{i + 1}", str(self.page.get_tag_amount()))
+        with open("config.ini", "w") as file:
+            self.data.config.write(file)
+
+    def list_for_notif(self):
+        """
+        creates a 2dim list with tag, old save of posts and 0
+
+        :0 value is changed in update(self)
+        :changes self.for_notif
+        """
+        self.data.config.read("config.ini")
+        list_tag = self.data.config.items("tags")
+        list_val = self.data.config.items("save_posts")
+        for i in range (len(list_tag)):
+            group = [list_tag[i][1], list_val[i][1], 0]
+            self.for_notif.append(group)
+    
+    def create_notifs(self):
+        """
+        creates a list of tuple with tag, and number of new posts
+
+        :changes self.notifs
+        """
+        self.notifs = []
+        self.data.config.read("config.ini")
+        list_tag = self.data.config.items("tags")
+        for i in range (len(list_tag)):
+            self.page.add_tag(list_tag[i][1])
+            self.page.calc_new_posts(int(self.for_notif[i][1]), self.for_notif[i][2])
+            self.notifs.append((list_tag[i][1], self.page.get_newposts()))
+
+    def reset_action(self):
+        """
+        resets the flag for action on the main window to None
+        """
+        self.ui.action_id = None
+    
+    def is_no_action(self):
+        """
+        checks if any action has been performed on the main window
+        """
+        return self.ui.action_id == None
+    
+    def launch(self):
+        if self.data.is_first_launch():
+            self.data.create_config()
+        if self.page.net_check() == False:
+            self.ui.print_message("Couldn't connect to rule34 ! Try again", "Error")
+            exit()
